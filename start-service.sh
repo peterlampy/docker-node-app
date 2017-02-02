b1b761a6741557b4ba463ae9f92c036507357113
David Yu <david.yu@docker.com>
2017-02-02 15:51:08 -0800

    docker 1.13 changes

--- start-service.sh
+++
@@ -1,3 +0,0 @@
-docker network create --driver overlay app-network
-docker service create --replicas 1 -p 27017:27017 --network app-network  --mount type=volume,destination=/data/db --constraint 'node.labels.workload == app' --name mongodb clusterhq/mongodb
-docker service create --replicas 3 -p 4000 -e MONGODB_SERVICE_SERVICE_HOST=mongodb --network app-network --network ucp-hrm --constraint 'node.labels.workload == app' --label com.docker.ucp.mesh.http=4000=http://nodeapp.david.dtcntr.net --name docker-node-app --with-registry-auth dtr.david.dtcntr.net/engineering/docker-node-app:latest
--- stop-service.sh
+++
@@ -1,3 +0,0 @@
-docker network rm app-network
-docker service rm mongodb
-docker service rm docker-node-app
